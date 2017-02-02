4817b6efb182ceed413c3db4f3736cd29713009b
Chris Lalancette <clalancette@osrfoundation.org>
2017-02-02 18:47:23 -0500

    Remove an unnecessary check from Converter::Remove.

---
+++ configure.bat
@@ -0,0 +1,12 @@
+@set build_type=Release
+@if not "%1"=="" set build_type=%1
+@echo Configuring for build type %build_type%
+
+@set IGN_MATH_PATH=%cd%\..\..\ign-math\build\install\%build_type%
+
+cmake -G "NMake Makefiles"^
+  -DCMAKE_PREFIX_PATH="%IGN_MATH_PATH%"^
+  -DBOOST_ROOT:STRING="%cd%\..\..\boost_1_56_0"^
+  -DBOOST_LIBRARYDIR:STRING="%cd%\..\..\boost_1_56_0\lib64-msvc-12.0"^
+  -DCMAKE_INSTALL_PREFIX="install/%build_type%"^
+  -DCMAKE_BUILD_TYPE="%build_type%" ..
