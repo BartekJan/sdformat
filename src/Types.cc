c87a601c7dd040cedeffe0f0e3582150eaa5bf91
chapulina <burajiru.no.chapulina@gmail.com>
2017-02-16 11:46:23 -0800

    style

---
+++ src/Types.cc
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2015-2017 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifdef _WIN32
+#include <Windows.h>
+#endif
+
+#include <string>
+#include <vector>
+
+#include "sdf/Types.hh"
+
+namespace sdf
+{
+/////////////////////////////////////////////////
+#ifdef _WIN32
+const char *winGetEnv(const char *_name)
+{
+  const DWORD buffSize = 65535;
+  static char buffer[buffSize];
+  if (GetEnvironmentVariable(_name, buffer, buffSize))
+  {
+    return buffer;
+  }
+  return NULL;
+}
+#else
+const char *winGetEnv(const char * /*_name*/)
+{
+  return NULL;
+}
+#endif
+
+/////////////////////////////////////////////////
+std::vector<std::string> split(const std::string &_str,
+                               const std::string &_splitter)
+{
+  std::vector<std::string> ret;
+  size_t next = 0;
+  size_t current = next;
+
+  while (next != std::string::npos)
+  {
+    next = _str.find(_splitter, current);
+    ret.push_back(_str.substr(current, next - current));
+    current = next + _splitter.length();
+  }
+
+  return ret;
+}
+}
